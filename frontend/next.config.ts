ecf921a3f50ddda1f4db935048533e6049a55e9d
dapo <adelanasimeone@gmail.com>
2026-06-01 11:11:05 +0100

    nextconfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Safely tells the Next.js compiler to ignore type errors during production builds
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Safely tells the Next.js compiler to ignore ESLint warnings during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
